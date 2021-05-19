2c89a580f7c478131fac9ca4647c376e6754f143
Mose <nekronmoseley@gmail.com>
2021-05-19 14:18:51 +0100

    Using Singleton.java

--- src/main/java/org/bships/plugin/ShipsMain.java
+++ src/main/java/org/bships/plugin/ShipsMain.java
@@ -3,7 +3,6 @@ package org.bships.plugin;
 import org.bukkit.Bukkit;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.core.CorePlugin;
 import org.core.command.CommandLauncher;
 import org.core.command.CommandRegister;
 import org.ships.config.configuration.ShipsConfig;
@@ -16,11 +15,15 @@ import java.util.Optional;
 
 public class ShipsMain extends JavaPlugin {
 
-    private ShipsBPlugin shipsPlugin;
-    private static ShipsMain plugin;
     public static final boolean INVENTORY_SLOT_INDEX_SHOW = true;
+    private static ShipsMain plugin;
+    private ShipsBPlugin shipsPlugin;
 
-    public void onEnable(){
+    public static ShipsMain getPlugin() {
+        return plugin;
+    }
+
+    public void onEnable() {
         plugin = this;
         Bukkit.getPluginManager().registerEvents(new DebugListener(), this);
         try {
@@ -32,9 +35,10 @@ public class ShipsMain extends JavaPlugin {
 
         }
         shipsPlugin = new ShipsBPlugin();
+        shipsPlugin.registerPlugin();
         CommandRegister register = new CommandRegister();
         shipsPlugin.registerCommands(register);
-        for(CommandLauncher command : register.getCommands()){
+        for (CommandLauncher command : register.getCommands()) {
             JavaPlugin plugin = (JavaPlugin) command.getPlugin().getLauncher();
             PluginCommand command2 = plugin.getCommand(command.getName());
             BCommand command3 = new BCommand(command);
@@ -43,6 +47,7 @@ public class ShipsMain extends JavaPlugin {
         }
 
         PluginCommand testCMD = Bukkit.getPluginCommand("shipstest");
+        shipsPlugin.registerReady();
 
         Bukkit.getScheduler().scheduleSyncDelayedTask(this, () -> {
             this.shipsPlugin.loadCustomShipType();
@@ -50,7 +55,7 @@ public class ShipsMain extends JavaPlugin {
             this.shipsPlugin.getLoadedMessages();
 
             ShipsConfig config = ShipsPlugin.getPlugin().getConfig();
-            if(config.isUpdateEnabled()) {
+            if (config.isUpdateEnabled()) {
                 ShipsUpdater updater = new ShipsUpdater();
                 Optional<ShipsUpdater.VersionInfo> opInfo = updater.shouldUpdate();
                 if (opInfo.isPresent()) {
@@ -63,11 +68,7 @@ public class ShipsMain extends JavaPlugin {
         });
     }
 
-    public void onDisable(){
+    public void onDisable() {
         this.shipsPlugin.getDebugFile().writeToDebug();
-    }
-
-    public static ShipsMain getPlugin(){
-        return plugin;
     }
 }
